016b8b511d39234970fe6678b370a10fc82df6da
Pablo Vergel <pvergel@themortercompany.com>
2014-03-30 20:48:40 +0200

    Initial release. C,R + Validations

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BlogRedux::Application.config.secret_key_base = '86aa21110c520f7ef3d357e5f3756cfcf99f32124725d1f6f55738bf15d6c75877a4105dda74bf6c3b1a77cfd8d2ae9d7d980b1fbb5a7c33ba32ecb1ed504d50'
